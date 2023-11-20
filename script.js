1898e899b7a82bad298fab3e164d4c2aa205f977
danny <ddjjpp340@gmail.com>
2023-11-19 23:53:50 -0330

    Added interact.js functionality

--- script.js
+++ script.js
@@ -131,6 +131,7 @@ function createGrid(x, y) {
   // Places pixelated image in grid-supercontainer
   var imageCreator = document.createElement("img");
   imageCreator.setAttribute("id", "grid-image");
+  imageCreator.setAttribute("class", "pixelated-image")
   imageCreator.src = pixelatedURL;
   gridSelector.appendChild(imageCreator);
 
@@ -171,3 +172,24 @@ function getGridSize() {
   console.log(`Height: ${imgHeight}`);
   console.log(`Width: ${imgWidth}`);
 }
+
+interact('.pixelated-image')
+  .resizable({
+    edges: {top: true, left: true, bottom: true, right: true},
+    listeners: {
+      move: function (event) {
+        let { x, y } = event.target.dataset
+
+        x = (parseFloat(x) || 0) + event.deltaRect.left
+        y = (parseFloat(y) || 0) + event.deltaRect.top
+
+        Object.assign(event.target.style, {
+          width: `${event.rect.width}px`,
+          height: `${event.rect.height}px`,
+          transform: `translate(${x}px, ${y}px)`
+        })
+
+        Object.assign(event.target.dataset, { x, y })
+      }
+    }
+  })
