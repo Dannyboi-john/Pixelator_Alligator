b6bd0f5279edc41e33090cd21ad79583d2fd3890
danny <ddjjpp340@gmail.com>
2023-11-21 00:20:46 -0330

    Made pixelated image opaque Added draggability (Not behaving as intended)

--- script.js
+++ script.js
@@ -172,6 +172,24 @@ function getGridSize() {
   console.log(`Width: ${imgWidth}`);
 }
 
+
+/*
+const position = { x: 0, y: 0 }
+interact('.pixelated-image').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
+
 interact('.pixelated-image')
   .resizable({
     edges: {top: true, left: true, bottom: true, right: true},
@@ -191,4 +209,57 @@ interact('.pixelated-image')
         Object.assign(event.target.dataset, { x, y })
       }
     }
+  })
+*/
+
+interact('.pixelated-image')
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+
+    listeners: {
+      move (event) {
+        var target = event.target
+        var x = (parseFloat(target.getAttribute('data-x')) || 0)
+        var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+        // update the element's style
+        target.style.width = event.rect.width + 'px'
+        target.style.height = event.rect.height + 'px'
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left
+        y += event.deltaRect.top
+
+        target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+        target.setAttribute('data-x', x)
+        target.setAttribute('data-y', y)
+        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+      }
+    },
+    
+    modifiers: [
+      // keep the edges inside the parent
+      interact.modifiers.restrictEdges({
+        outer: 'grid-container'
+      }),
+
+      // minimum size
+      interact.modifiers.restrictSize({
+        min: { width: 100, height: 50 }
+      })
+    ],
+    
+    
+  })
+  .draggable({
+    listeners: { move: window.dragMoveListener },
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'grid-container',
+        endOnly: true
+      })
+    ]
   })
