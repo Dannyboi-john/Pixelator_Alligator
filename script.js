8706dbabaa9aa2f757f6cac1f156502e05163784
danny <ddjjpp340@gmail.com>
2023-11-22 20:16:27 -0330

    Cleaned up  a few redundant comments

--- script.js
+++ script.js
@@ -189,28 +189,6 @@ interact('.pixelated-image').draggable({
     },
   }
 })
-/*
-interact('.pixelated-image')
-  .resizable({
-    edges: {top: true, left: true, bottom: true, right: true},
-    listeners: {
-      move: function (event) {
-        let { x, y } = event.target.dataset
-
-        x = (parseFloat(x) || 0) + event.deltaRect.left
-        y = (parseFloat(y) || 0) + event.deltaRect.top
-
-        Object.assign(event.target.style, {
-          width: `${event.rect.width}px`,
-          height: `${event.rect.height}px`,
-          transform: `translate(${x}px, ${y}px)`
-        })
-
-        Object.assign(event.target.dataset, { x, y })
-      }
-    }
-  })
-*/
 
 interact('.pixelated-image')
   .resizable({
