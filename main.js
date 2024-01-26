2ca30c23b1163836d6da84192ee967e9f07cab5b
danny <ddjjpp340@gmail.com>
2024-01-26 15:40:30 -0330

    Can now hold down mouse to draw on grid (when it's just the grid)

--- main.js
+++ main.js
@@ -184,9 +184,21 @@ function gridStandalone(x, y) {
   $(".grid").height(gridSelector.offsetHeight / y);
 
   // Changes the square to black on click.
-  $(".grid").click(function() {
+  var isDown = false;
+  $(".grid").mousedown(function() {
+    isDown = true;
+  })
+  $(".grid").mouseup(function() {
+    isDown = false;
+  })
+  $(".grid").mouseover(function() {
+    if(isDown) {
       $(this).toggleClass("clicked-grid");
+    }
   });
+  $(".grid").click(function() {
+    $(this).toggleClass("clicked-grid");
+  })
 }
 
 function clearGrid() {
