0a36fe757fc5731eab0f834e0049a5377707bf5a
danny <ddjjpp340@gmail.com>
2024-02-16 15:33:30 -0330

    Browse now properly reappears on drag leave

--- drag-leave-handler.js
+++ drag-leave-handler.js
@@ -2,5 +2,12 @@ export function dragLeaveHandler(ev) {
   //Changes text within photo-reciever back to normal.
   ev.preventDefault();
   const dragText = document.querySelector('.photo-reciever');
-  dragText.textContent = 'Drag and drop images here!';
+  dragText.innerHTML = `Drag and drop images here! or <span class="browse">browse</span> <input type="file" id="browse-images" hidden/></div>`;
+
+  let browse = document.querySelector(".browse");
+  let input = document.getElementById("browse-images")
+
+  browse.onclick = () => {
+    input.click();
+  };
   };
